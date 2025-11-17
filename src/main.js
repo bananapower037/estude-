9679220658c8f6f6f8b536821c055d8eb121503e
Venomxtreme <1066812@senacminas.edu.br>
2025-11-17 16:18:26 -0300

    configuração do firebase

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import './assets/main.css'
+import './plugins/firebase.js'
 import router from './router';
 
 const app = createApp(App);
---
+++ src/plugins/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC0asRqYDkfW41RZn3JD6_wuz5q8zjxAsk",
+  authDomain: "estude-mais-c44ae.firebaseapp.com",
+  projectId: "estude-mais-c44ae",
+  storageBucket: "estude-mais-c44ae.firebasestorage.app",
+  messagingSenderId: "949727975468",
+  appId: "1:949727975468:web:e0842f9c0419a342d4d989"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
